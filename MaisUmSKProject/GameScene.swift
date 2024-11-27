6b6952c493c18455850ba88c52931625b5219418
Francisco Soares <fmssn@cin.ufpe.br>
2024-11-27 17:25:42 -0300

    Quick and dirty recreation of SpriteKitPong.
    Still buggy, only first commit.

--- MaisUmSKProject/GameScene.swift
+++ MaisUmSKProject/GameScene.swift
@@ -9,80 +9,88 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
-    
+
+    private var moveableNode: SKNode?
+
+    var ballNode: SKNode?
+
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        self.physicsWorld.contactDelegate = self
+
+        guard let ballNode = self.childNode(withName: "ball") else { return }
+
+        ballNode.physicsBody?.applyImpulse(CGVector(dx: 20.0, dy: 0.0))
+        self.ballNode = ballNode
+
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+
+    // MARK: Touch functions
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let label = self.label {
             label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        for touch in touches {
+            let point = touch.location(in: self)
+            let nodes = self.nodes(at: point)
+
+            for node in nodes {
+                if node == self {
+                    print("There's self")
+                } else {
+                    if node != ballNode {
+                        moveableNode = node
+                    }
+                }
+            }
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        if let moveableNode {
+            var location: CGPoint? = nil
+
+            for touch in touches {
+                location = touch.location(in: self)
+            }
+
+            if let location {
+                moveableNode.position.y = location.y
+            }
+        }
     }
-    
+
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        self.moveableNode = nil
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        print("oops, touch cancelled!")
+
+        self.moveableNode = nil
     }
-    
-    
+
+    // MARK: update function
+
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+}
+
+// -MARK: Contact Delegate
+
+extension GameScene: SKPhysicsContactDelegate {
+    func didBegin(_ contact: SKPhysicsContact) {
+        print("Contact started between \(contact.bodyA) and \(contact.bodyB)")
+    }
+
+    func didEnd(_ contact: SKPhysicsContact) {
+        print("Contact ended between \(contact.bodyA) and \(contact.bodyB)")
     }
 }
